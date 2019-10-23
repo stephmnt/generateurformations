843686b0a37fcf6adb27f4a61272cc3545076fb3
Stéphane <stephane@libre-a-toi.org>
2019-10-23 21:55:18 +0200

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #Générateur d'objectif pédagogique bullshit
-À force de répondre tout le temps aux mêmes appels d'offres, on finit par se simplifier la vie. 
+
+À force de répondre tout le temps aux mêmes appels d'offres, on finit par se simplifier la vie.
--- main.js
+++ main.js
@@ -5,12 +5,12 @@ let citation = document.querySelector('.objectif');
 
 
 
-// Ensemble de phrase animaux vs hommes
+// Objectifs pédagogiques générés en trois partie
 const sujet = ["l'innovation", "un dispositif blended learning", "les cultures numériques en formation", "de nouvelles pratiques pédagogiques", "sa propre posture de formateur", "les plus-values et les limites de la ré-ingénierie d'une pédagogie digitale", "différentes pratiques pédagogiques"];
 const verbe = ["Identifier", "Concevoir", "Réaliser", "Produire", "Analyser", "Expérimenter"];
 const comp = ["et y faire face", "en management distruptif", "dans une démarche créativité", "en intégrant le numérique", "dans un environnement numérique","en vue d'une évaluation","en fonction du contexte de la formation", "en analysant la pertinence pédagogique"];
 
-// Fonction pour choisir un leement d'un tableau de facon aleatoire
+// Fonction pour choisir un element du tableau de facon aleatoire
 function aleatoire(arr) {
     return arr[Math.floor(Math.random() * arr.length )];
 }
