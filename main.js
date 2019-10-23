b3b09f5fb39572a3ed910c539a66f1e29d1effab
Stéphane <stephane@libre-a-toi.org>
2019-10-23 21:53:10 +0200

    maj

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let citation = document.querySelector('.objectif');
 
 
 // Ensemble de phrase animaux vs hommes
-const sujet = ["l'innovation", "un dispositif blended learning", "les cultures numériques en formation", "de nouvelles pratiques pédagogiques", "sa propre posture de formateur", "les plus-values et les limites de la ré-ingénierie d'une pédagogie digitale", "différentes pratiques pédagogiques", ""];
+const sujet = ["l'innovation", "un dispositif blended learning", "les cultures numériques en formation", "de nouvelles pratiques pédagogiques", "sa propre posture de formateur", "les plus-values et les limites de la ré-ingénierie d'une pédagogie digitale", "différentes pratiques pédagogiques"];
 const verbe = ["Identifier", "Concevoir", "Réaliser", "Produire", "Analyser", "Expérimenter"];
 const comp = ["et y faire face", "en management distruptif", "dans une démarche créativité", "en intégrant le numérique", "dans un environnement numérique","en vue d'une évaluation","en fonction du contexte de la formation", "en analysant la pertinence pédagogique"];
 
