2380de5859c9b16462ce1a6e4540bc99aea860f4
Stéphane <stephane@libre-a-toi.org>
2019-10-23 22:15:30 +0200

    explications et commentaires

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-#Générateur d'objectif pédagogique bullshit
+# Générateur d'objectifs pédagogiques bullshit
 
 À force de répondre tout le temps aux mêmes appels d'offres, on finit par se simplifier la vie.
+
+Le code est structuré autour d'un tableau en 3 lignes et quelques colonnes. Les lignes représentent les différentes parties d'un objectif pédagogique en formation des adultes (un verbe d'action, une réalisation et un contexte), et les différentes colonnes contiennent une proposition pour chaque.
+
+Le script pioche aléatoirement une occurrence afin de générer un objectif pédagogique.
+
+La probabilité fait que certains de ces objectifs seront vraisemblables, voir pertinents ; d'autres seront parfaitement absurdes... Amusez-vous bien !
+
+PS: Il est possible de me proposer d'enrichir le tableau de propositions différentes [en cliquant ici](https://github.com/stephmnt/generateurformations/issues/new) ! 
--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-// Mes boutons
+// Le bouton
 const ba = document.querySelector('#generer');
 let citation = document.querySelector('.objectif');
 
-
-
-
-// Objectifs pédagogiques générés en trois partie
+// Objectifs pédagogiques générés en trois parties
 const sujet = ["l'innovation", "un dispositif blended learning", "les cultures numériques en formation", "de nouvelles pratiques pédagogiques", "sa propre posture de formateur", "les plus-values et les limites de la ré-ingénierie d'une pédagogie digitale", "différentes pratiques pédagogiques"];
 const verbe = ["Identifier", "Concevoir", "Réaliser", "Produire", "Analyser", "Expérimenter"];
 const comp = ["et y faire face", "en management distruptif", "dans une démarche créativité", "en intégrant le numérique", "dans un environnement numérique","en vue d'une évaluation","en fonction du contexte de la formation", "en analysant la pertinence pédagogique"];
@@ -15,7 +12,7 @@ function aleatoire(arr) {
     return arr[Math.floor(Math.random() * arr.length )];
 }
 
-// Actions des boutons
+// Actions du bouton
 ba.addEventListener('click', () => {
 // Action du bouton effacer
 citation.innerHTML = aleatoire(verbe)+ " " + aleatoire(sujet)+ " "+ aleatoire(comp)+ ".";
